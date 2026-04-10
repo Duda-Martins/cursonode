1dfe10e7765a66673643b8b2b40c8e13f3d5ec23
Maria Eduarda <mrodrigues.mariaeduarda@gmail.com>
2026-04-10 16:20:54 -0300

    Funções síncronas e assíncronas.

---
+++ fs-sync.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+console.log((process.hrtime()[0]/60).toFixed(5));
+console.log("Antes da leitura do arquivo");
+
+const dados = fs.readFileSync('file.txt', 'utf8');
+
+console.log("Executando o console após o arquivo");
+
+console.log((process.hrtime()[0]/60).toFixed(5));
