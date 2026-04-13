38c747a75308696045166af82474590b0bb0002d
Maria Eduarda <mrodrigues.mariaeduarda@gmail.com>
2026-04-13 16:26:42 -0300

    Mais sobre eventos.

--- events/event.js
+++ events/event.js
@@ -1,10 +1,19 @@
-const EventEmitter = require('events');
+const {EventEmitter} = require('events');
 
 class Event extends EventEmitter {}
 const meuEvento = new Event();
 
+
+// subscriber - assinante
 meuEvento.on('segurança', (x,y) => {
   console.log(`Executando o evento 'segurança': ${x} e ${y}`);
 });
 
+// publisher - emissor
 meuEvento.emit('segurança', 'userAdimin', 'alterou Salário');
+
+meuEvento.on("encerrar", (dados) => {
+  console.log(`Assinante ${dados}`);
+});
+
+meuEvento.emit("encerrar", "Encerrando a execução da importação dos dados!");
